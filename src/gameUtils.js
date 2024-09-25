5d112c38dbd8dfc58a8548eb09ddfba4c3954518
Lev <wleow.ershov@gmail.com>
2024-09-25 10:55:17 +0300

    fix liner after CE part 5

--- src/gameUtils.js
+++ src/gameUtils.js
@@ -1,3 +1,5 @@
-export default getRandomNumber = (min = 0, max = 100) => {
+const getRandomNumber = (min = 0, max = 100) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export default getRandomNumber;
